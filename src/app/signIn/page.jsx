e25233feae6800ef5609996e636d65dc57de28df
23joy <joyshreeroy288@gmail.com>
2026-05-18 08:24:14 -0700

    add the login page

---
+++ src/app/signIn/page.jsx
@@ -0,0 +1,59 @@
+'use client'
+import { Button, Card, Description, FieldError, Fieldset, Form, Input, Label, TextField } from '@heroui/react';
+import React from 'react';
+import { LuLogIn } from 'react-icons/lu';
+
+const page = () => {
+    return (
+        <Card className='max-w-2xl mx-auto mt-10'>
+            <h2 className='text-center p-5 font-bold text-2xl'>Login Now</h2>
+            <Form>
+                <Fieldset className="w-full">
+                    <Fieldset.Group>
+                        <TextField isRequired name="email" type="email">
+                            <Label>Email</Label>
+                            <Input placeholder="john@example.com" variant="secondary" className={'rounded-none'} />
+                            <FieldError />
+                        </TextField>
+
+                        <TextField
+                            isRequired
+                            minLength={8}
+                            name="password"
+                            type="password"
+                            validate={(value) => {
+                                if (value.length < 8) {
+                                    return "Password must be at least 8 characters";
+                                }
+                                if (!/[A-Z]/.test(value)) {
+                                    return "Password must contain at least one uppercase letter";
+                                }
+                                if (!/[0-9]/.test(value)) {
+                                    return "Password must contain at least one number";
+                                }
+                                return null;
+                            }}
+                        >
+                            <Label>Password</Label>
+                            <Input placeholder="Enter your password" className={'rounded-none'} variant='secondary' />
+                            <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+                            <FieldError />
+                        </TextField>
+                    </Fieldset.Group>
+
+                    <Fieldset.Actions className='flex flex-col'>
+                        <Button type="submit" className={'w-full'}>
+                            <LuLogIn />
+                            Login
+                        </Button>
+                        <li className={'w-full hover:bg-separator-secondary p-2 rounded-2xl text-center'}>
+                            Forgotten Password?
+                        </li>
+                    </Fieldset.Actions>
+                </Fieldset>
+            </Form>
+        </Card>
+    );
+};
+
+export default page;
